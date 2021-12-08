83a452aa6dd48fd3b154d0e4b35f2c3226644701
brenoAmodesto <brenoamodesto58@gmail.com>
2021-12-07 23:35:08 -0300

    main

--- k8s-projeto-continuo/backend.tf
+++ k8s-projeto-continuo/backend.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
         region = "us-east-1"
-        bucket = "k8s-backend-video"
+        bucket = "k8s-backend"
         encrypt = "true"
         key = "terraform.tfstate"
     }
--- k8s-projeto-continuo/main.tf
+++ k8s-projeto-continuo/main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "sa-east1"
 }
 
 resource "aws_key_pair" "k8s-key" {
   key_name   = "k8s-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDFdhihtJ/ihWPMJsb8ynlLYbzetYpfrlkcSL5Bca92qxq4XRZ8UFLOu9G4CxpbHUNaA3BL0CbrtrGMonOyWqlT8CPzcbNehO6FRPn0aQpkljdJOVJb+yv4suSH504BRpxHYW8ZF2WAcND8g5/ojeyu97qOSBtkE+oBjCA5VoT3DvJINZIJ86Ec1ns/wAGq/VcefbkPkYWD3s9o34vMrDaYYezYLzVUThxntB1ZO0mrXM0KM/vY3L67PDoCFEvK3JwVvl4koajey+j4M+aprN8IA29Hd1RwvhFsS4Qturqk2gnctQamyFYjmGskUhiuhGaAQcRGfG9VqFn3sMJ4GA8UbSgeSPSrutGwa6DLGmvTJcORS9v786P7VxvDNGXJfpRZheZUnQgNscAR7MH9HldpijmFrMetjfcb5dLidxW9RRiMfR3kfyW0mO5M6GriPjqNH9cTbZGaDYphir23MS+0tSANR2mHR+ECrpaXOpykMzZGg4QxJTQpd/ljESg05xk= ubuntu@i-09aed0533d7441e77"
+  public_key = "chave"
 }
 
 resource "aws_security_group" "k8s-sg" {
