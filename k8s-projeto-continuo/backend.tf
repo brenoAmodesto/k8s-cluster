beea98e9dab63a5f132833b1ecfa7cd96be55ab8
brenoAmodesto <brenoamodesto58@gmail.com>
2021-12-07 23:35:56 -0300

    main

--- k8s-projeto-continuo/backend.tf
+++ k8s-projeto-continuo/backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        region = "us-east-1"
+        region = "sa-east-1"
         bucket = "k8s-backend"
         encrypt = "true"
         key = "terraform.tfstate"
